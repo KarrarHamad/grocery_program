79c94aca2194411d20cf13b0a67c023188b5847a
Ali <alihamad313313@gmail.com>
2025-09-27 17:37:17 +0300

    Adding Notes Before Program

---
+++ grocery_program.py
@@ -0,0 +1,10 @@
+# consider a dictionary of grocery items with their prices
+# Continuosly ask the user to input what they want to buy, and stop when they type "done"
+# If the item is in the dictionary, add it to a list of items to buy
+# If the item is not in the dictionary, print a message saying "Sorry, we don't have that item."
+# After finishing, print the total cost of the items in the list using a loop (use the prices that i like)
+# if the If the total is greater than $10, print "You spent a lot!", otherwise print "You spent a little!"
+# Print the final list of items and the total cost.
+# Let the user type a quantity after the item (example: apple 3). Update your code so it multiplies the price by the quantity.
+# Save the cart to a dictionary like: cart = {"apple": 3, "milk": 1}
+# Add a discount rule: if "milk" is bought more than 2 times, reduce the price by $1.
